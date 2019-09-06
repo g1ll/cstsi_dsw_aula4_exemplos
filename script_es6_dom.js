943ed2b9158acc3e44feb4591777c840878e4492
Gill <gillvelleda@gmail.com>
2019-09-06 08:27:45 -0300

    Correção do Trabalho

---
+++ script_es6_dom.js
@@ -0,0 +1,168 @@
+ window.onload = () => {
+     document.cadastro.addEventListener('submit', processaCadastro);
+ };
+
+ const listProds = []
+ let prodId = 0
+ let prodIdedit = 0
+
+ function processaCadastro(e) {
+     e.preventDefault();
+     const submit = document.querySelector('input[type="submit"]')
+     const titulo = document.querySelectorAll('h3')[0]
+     const selectdesco = this.desco.selectedOptions;
+
+     const listItens = []
+     Array.from(this.itens).forEach((item) => {
+         if (item.checked) listItens.push(item)
+     });
+
+     const produto = {
+         id: (!prodIdedit) ? ++prodId : prodIdedit,
+         nome: this.nome.value,
+         desc: this.desc.value,
+         qtd: this.qtd.value,
+         preco: this.preco.value,
+         origem: this.ori.value,
+         itens: (listItens.length) ? listItens : [{ value: 'Sem Itens' }],
+         desco: (selectdesco.length) ? selectdesco : [{ value: 'Sem Descontos' }]
+     }
+
+     if (!prodIdedit) { //Cadastrar
+         listProds.push(produto)
+         addTable(produto)
+     } else { //Salvar
+         listProds.find((prod, i) => {
+             if (prod.id === prodIdedit) {
+                 listProds[i] = produto;
+                 prodIdedit = 0;
+                 return true
+             };
+         });
+         editTable(produto)
+         this.setAttribute('style', '')
+         titulo.setAttribute('style', '')
+     }
+     titulo.innerHTML = 'Cadastrar Produto'
+     submit.value = 'Cadastrar'
+     this.reset()
+ }
+
+ function removeProd() {
+     if (confirm('Você excluirá este item definitivamente!')) {
+         let idpro = parseInt(this.getAttribute('idpro'));
+         listProds.splice(listProds.findIndex(prod => {
+             if (prod.id === idpro) {
+                 removeTable(idpro);
+                 return true;
+             }
+         }), 1);
+     }
+ }
+
+ function editProd() {
+     const idpro = Number(this.getAttribute('idpro'));
+     const prod = listProds.find((prod, index) => {
+         if (prod.id === idpro) {
+             return true;
+         }
+     });
+     if (prod) {
+         const cadastro = document.cadastro
+         cadastro.nome.value = prod.nome
+         cadastro.desc.value = prod.desc
+         cadastro.qtd.value = prod.qtd
+         cadastro.preco.value = prod.preco
+         if (prod.origem)
+             cadastro.ori[(prod.origem === 'Nacional') ? 0 : 1].checked = true
+         const titulo = document.querySelectorAll('h3')[0]
+         titulo.innerHTML = 'Editar Produto'
+         titulo.setAttribute('style', 'color:red')
+         cadastro.submit.value = 'Salvar'
+         prodIdedit = prod.id
+         cadastro.nome.focus()
+         cadastro.setAttribute('style', 'border:solid 2px red;border-radius:10px')
+     }
+ }
+
+ function addTable(produto) {
+     if (produto) {
+         const table = document.querySelector('tbody')
+         const tr = document.createElement('tr')
+         const listTds = new Array()
+
+         let td, ul, li
+
+         tr.setAttribute('id', produto.id)
+
+         for (let [key, prod] of Object.entries(produto)) {
+             td = document.createElement('td')
+             if (key === 'desco' || key === 'itens') {
+                 ul = document.createElement('ul')
+                 Array.from(prod).forEach(item => {
+                     li = document.createElement('li')
+                     li.appendChild(document.createTextNode(item.value))
+                     ul.appendChild(li)
+                 })
+                 td.appendChild(ul)
+             } else {
+                 td.appendChild(document.createTextNode(prod))
+             }
+             listTds.push(td)
+         }
+
+         const remove = document.createElement('button')
+         remove.innerHTML = '&#128465;' //&#10000
+         remove.setAttribute('idpro', '' + produto.id)
+         remove.setAttribute('title', 'Remover este item.')
+         remove.addEventListener('click', removeProd)
+
+         const edit = document.createElement('button')
+         edit.innerHTML = '&#10000;'
+         edit.setAttribute('idpro', '' + produto.id)
+         edit.setAttribute('title', 'Editar este item.')
+         edit.addEventListener('click', editProd)
+
+         td = document.createElement('td')
+         td.appendChild(remove)
+         td.appendChild(edit)
+
+         listTds.push(td)
+
+         listTds.forEach((td, key) => {
+             tr.appendChild(td)
+         })
+
+         table.appendChild(tr)
+     }
+ }
+
+ function removeTable(id) {
+     var table = document.querySelector('tbody')
+     table.removeChild(document.getElementById(id))
+ }
+
+ function editTable(produto) {
+     if (produto) {
+         const tr = document.getElementById(produto.id)
+         const tdAcoes = tr.childNodes[tr.childElementCount - 1]
+         while (tr.lastChild) tr.removeChild(tr.lastChild);
+         let td, ul, li;
+         for ([key, prod] of Object.entries(produto)) {
+             td = document.createElement('td');
+             if (key === 'desco' || key === 'itens') {
+                 ul = document.createElement('ul')
+                 Array.from(prod).forEach(item => {
+                     li = document.createElement('li')
+                     li.appendChild(document.createTextNode(item.value))
+                     ul.appendChild(li)
+                 });
+                 td.appendChild(ul)
+             } else {
+                 td.innerHTML = prod
+             }
+             tr.appendChild(td)
+         }
+         tr.appendChild(tdAcoes)
+     }
+ }
